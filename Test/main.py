e37044764a45e554390b1276c13c09b00d56bb13
Alvaro Monserrat <alvaro.monserrat@hotmail.com>
2019-01-04 09:49:06 -0300

    Update

--- Test/main.py
+++ Test/main.py
@@ -7,6 +7,20 @@ print("1")
 a = int(input("Ingrese un primer numero "))
 b = int(input("Ingrese un segundo numero "))
 
+# Atribucion Multiple
+
+a, b = b, a
+
+x, y, z = 3, 4, 5
+x, y, z = x**2, x+y+z, x*y*z
+
+print("x es " + str(x))
+print("y es " + str(y))
+print("z es " + str(z))
+"""
+-------------------------------------
+"""
+
 if a > b:
     print("El primer numero %d es mayor que el segundo %d." %(a, b))
 elif a < b:
