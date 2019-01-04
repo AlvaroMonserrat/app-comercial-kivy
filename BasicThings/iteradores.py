caafb506934148df93c5e20ff135569284012ddd
Alvaro Monserrat <alvaro.monserrat@hotmail.com>
2019-01-04 11:12:49 -0300

    Update

--- BasicThings/iteradores.py
+++ BasicThings/iteradores.py
@@ -3,8 +3,19 @@
 Capitulo de Iteraciones
 """
 
-# Atribuciones Condicionales
+"""------------------------------------------------------
+# Atribuciones Condicionales (Otra forma de if ():)
+
 value_t = int(input("Ingrese un número mayor que 0: "))
 s = "Par" if value_t % 2 == 0 else "Impar"
 
 print("El número ingresado es " + s)
+
+------------------------------------------------------"""
+
+# while
+
+x = 1
+while x <= 10:
+    print(x)
+    x += 1
