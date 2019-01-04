1ae1f193dd6fb5e0ba7443af0129122208ce6886
Alvaro Monserrat <alvaro.monserrat@hotmail.com>
2019-01-04 11:01:51 -0300

    Update

---
+++ BasicThings/iteradores.py
@@ -0,0 +1,10 @@
+"""
+---CURSO DE PYTHON--
+Capitulo de Iteraciones
+"""
+
+# Atribuciones Condicionales
+value_t = int(input("Ingrese un número mayor que 0: "))
+s = "Par" if value_t % 2 == 0 else "Impar"
+
+print("El número ingresado es " + s)
