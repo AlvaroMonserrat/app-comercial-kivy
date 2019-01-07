cd2bab4def41d943bd3c4a5905abe521a0f5ff1c
Alvaro Monserrat <alvaro.monserrat@hotmail.com>
2019-01-07 01:03:41 -0300

    Update

--- BasicThings/iteradores.py
+++ BasicThings/iteradores.py
@@ -3,19 +3,39 @@
 Capitulo de Iteraciones
 """
 
-"""------------------------------------------------------
 # Atribuciones Condicionales (Otra forma de if ():)
-
+"""------------------------------------------------------
 value_t = int(input("Ingrese un número mayor que 0: "))
 s = "Par" if value_t % 2 == 0 else "Impar"
 
 print("El número ingresado es " + s)
-
 ------------------------------------------------------"""
 
 # while
-
+"""------------------------------------------------------
 x = 1
 while x <= 10:
     print(x)
     x += 1
+else:
+    print("Fin del Ciclo")
+------------------------------------------------------"""
+
+
+# for var> in <lista> and function -range([start],stop[,step])- -range(stop[)-
+"""------------------------------------------------------
+for c in range(10, 0, -1):
+    if c % 2 == 0:
+        continue
+    print(c)
+    if c == 5:
+        break
+------------------------------------------------------"""
+# Estructura de Datos (7 principales)
+# Lista
+# Pila
+# Array / Matriz / Vector
+# Tupla
+# Set(Grupo)
+# Diccionario
+# Arbol
